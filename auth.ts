d1b77f1cc9b41d5c6f385e8f79aecfdedf2a3a09
Jose Ortiz <jortiz@disruptiveadvertising.com>
2024-01-24 09:51:46 -0700

    Fixed authentication and invoice deletion

--- auth.ts
+++ auth.ts
@@ -18,21 +18,25 @@ async function getUser(email: string): Promise<User | undefined> {
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
-  providers: [Credentials({
-    async authorize(credentials) {
-      const parsedCredentials = z
-        .object({ email: z.string().email(), password: z.string().min(6) })
-        .safeParse(credentials);
+  providers: 
+    [Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
 
-      if (parsedCredentials.success) {
-        const { email, password } = parsedCredentials.data;
-        const user = await getUser(email);
-        if (!user) return null;
-        const passwordsMatch = await bcrypt.compare(password, user.password);
-      }
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
 
-      console.log('Invalid credentials');
-      return null;
-    },
-  }),],
+          if (passwordsMatch) return user;
+        }
+
+        console.log('Invalid credentials');
+        return null;
+      },
+    }),
+  ],
 });
